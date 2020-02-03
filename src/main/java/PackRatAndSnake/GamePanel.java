8c723bb8ade9ae9ee2a78d0524db8750a9c37744
Benjamin Rogers <benji153@gmail.com>
2020-02-02 21:52:29 -0500

    Added basic movement to rat character

--- src/main/java/PackRatAndSnake/GamePanel.java
+++ src/main/java/PackRatAndSnake/GamePanel.java
@@ -7,6 +7,8 @@ package PackRatAndSnake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,12 +22,38 @@ public class GamePanel extends JPanel implements Runnable{
     
     boolean gameRunning = true;
     Thread game;
-    Color setColor = Color.black;
-    Random random = new Random();
+    PackRat player;
     
     public GamePanel(){
         game = new Thread(this);
         game.start();
+        player = new PackRat(0,0,20,20);
+        this.setFocusable(true);
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int id = e.getKeyCode();
+                if(id == (KeyEvent.VK_W)){
+                    player.currentDirection=1;
+                }else if(id == (KeyEvent.VK_D)){
+                    player.currentDirection=2;
+                }else if(id == (KeyEvent.VK_S)){
+                    player.currentDirection=3;
+                }else if(id == (KeyEvent.VK_A)){
+                    player.currentDirection=4;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                
+            }
+        });
     }
 
     @Override
@@ -33,7 +61,7 @@ public class GamePanel extends JPanel implements Runnable{
         while(gameRunning){
             
             try {
-                Thread.sleep(1000/6);
+                Thread.sleep(1000/60);
                 repaint();
                 update();
             } catch (InterruptedException ex) {
@@ -45,12 +73,17 @@ public class GamePanel extends JPanel implements Runnable{
     
     @Override
     public void paintComponent(Graphics g){
-        g.setColor(setColor);
+        g.setColor(Color.BLACK);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        draw(g);
+    }
+    
+    public void draw(Graphics g){
+        player.draw(g);
     }
     
     public void update(){
-        setColor = new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255));
+        player.update();
     }
     
 }
---
+++ src/main/java/PackRatAndSnake/PackRat.java
@@ -0,0 +1,48 @@
+
+package PackRatAndSnake;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+
+public class PackRat {
+   
+    public int speed = 3;
+    public Rectangle bounds;
+    public int currentDirection = 0;
+    public int currentPellets = 0;
+    
+    
+    public PackRat(int x,int y, int width ,int height){
+        this.bounds = new Rectangle(x,y,width,height);
+    }
+    
+    public void draw(Graphics g){
+        g.setColor(new Color(150,75,0));
+        g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+    
+    public void update(){
+        switch(currentDirection){
+                case 0:
+                    break;
+                case 1:
+                    this.bounds.y-=speed;
+                    break;
+                case 2:
+                    this.bounds.x+=speed;
+                    break;
+                case 3:
+                    this.bounds.y+=speed;
+                    break;
+                case 4:
+                    this.bounds.x-=speed;
+                    break;
+                default:
+                    this.currentDirection=0;
+                    break;
+        }
+    }
+    
+}
